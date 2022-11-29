7f3d76441c83d47318366ac6cfed1bba1e026c0b
David Aydin <david.aydin6@gmail.com>
2022-11-28 20:43:39 -0500

    removed duplicating backgrounds

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,24 @@ import React from "react";
 import "../assets/css/home.css";
 
 function Home() {
-    return(
-        <div class="home" align="center">
-            <h1>How does this app work?</h1>
-            <p>Click or Tap on the buttons above to begin finding your next drink!</p>
-            <p>Use the search function to filter drinks to your liking or jump be adventurous and try something new at random!</p>
+  return (
+    <div class="home" align="center">
+      <h1>How does this app work?</h1>
+      <p>Click or Tap on the buttons above to begin finding your next drink!</p>
+      <p>
+        Use the search function to filter drinks to your liking or jump be
+        adventurous and try something new at random!
+      </p>
 
-            <div class="about-me">
-                <h4>
-                    <a href="https://www.linkedin.com/in/daydin14/" target="_blank">Created by David Aydin © 2022</a>
-                </h4>
-            </div>
-        </div>
-    );
+      <div class="about-me" align="center">
+        <h4>
+          <a href="https://www.linkedin.com/in/daydin14/" target="_blank">
+            Created by David Aydin © 2022
+          </a>
+        </h4>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
